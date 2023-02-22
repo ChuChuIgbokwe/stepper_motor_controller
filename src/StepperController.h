d8cd847bf6e188be9d03b88646222deccb1edaf5
Chu-Chu Igbokwe <raider64x@gmail.com>
2023-02-22 00:08:32 -0800

    making it more modular

--- src/StepperController.h
+++ src/StepperController.h
@@ -4,40 +4,67 @@
 
 #ifndef URBAN_MACHINE_GENERIC_STEPPER_MOTOR_CONTROLLER_STEPPERCONTROLLER_H
 #define URBAN_MACHINE_GENERIC_STEPPER_MOTOR_CONTROLLER_STEPPERCONTROLLER_H
-
+#include <iostream>
+#include <math.h>
+#include <fstream>
 
 class StepperController {
 public:
     //Constructor
-    StepperController();
-    StepperController(int initial_position, int initial_velocity);
+//    StepperController();
+//    StepperController(int initial_position, int initial_velocity);
+    StepperController(float initial_position, float initial_velocity);
 
-    float getCurrentPosition();
+    float getCurrentPosition() const;
 
-    float getCurrentVelocity();
+    float getCurrentVelocity() const;
 
-    void set_goal(int position, int velocity, int acceleration);
+//    void set_goal(int position, int velocity, int acceleration);
+    void set_goal(float position, float velocity, float acceleration);
 
-    void step(){}
+    void step();
 
     bool pre_motion_sanity_checks(float initial_position, float initial_velocity, float
     goal_position, float max_velocity, float max_acceleration);
 
-    void print_values();
-
 private:
-    int _initial_position;
-    int _initial_velocity;
-    int _current_position;
-    int _current_velocity;
-    int _current_acceleration;
-    int _goal_position;
-    int _max_velocity;
-    int _max_acceleration;
+    float _initial_position;
+    float _initial_velocity;
+    float _current_position;
+    float _current_velocity;
+    float _current_acceleration;
+    float _goal_position;
+    float _max_velocity;
+    float _max_acceleration;
+    float _acceleration_time;
+    float _acceleration_distance;
+    float _deceleration_time;
+    float _deceleration_distance;
+    float _total_time;
+    float _total_distance;
+    float _cruising_time;
+    float _cruising_distance;
+    float _remaining_distance;
+    float _direction;
 
+
+    void print_acceleration_debug_values();
+
+    void _set_direction();
+
+    void _calculate_distances();
+
+    void _calculate_times();
+
+    float calculate_acceleration_time(float max_velocity, float initial_velocity, float max_acceleration);
+
+    float calculate_acceleration_distance(float initial_velocity, float acceleration_time, float max_acceleration);
+
+    float calculate_deceleration_time(float max_velocity, float max_acceleration);
+
+    float calculate_deceleration_distance(float max_acceleration, float deceleration_time);
 };
 
-};
 
 
 #endif //URBAN_MACHINE_GENERIC_STEPPER_MOTOR_CONTROLLER_STEPPERCONTROLLER_H
