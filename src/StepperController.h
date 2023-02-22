da08800f36073c82422c08f9911c4d718b0e149d
Chu-Chu Igbokwe <raider64x@gmail.com>
2023-02-21 21:47:20 -0800

    added methods and private variables

--- src/StepperController.h
+++ src/StepperController.h
@@ -7,6 +7,35 @@
 
 
 class StepperController {
+public:
+    //Constructor
+    StepperController();
+    StepperController(int initial_position, int initial_velocity);
+
+    float getCurrentPosition();
+
+    float getCurrentVelocity();
+
+    void set_goal(int position, int velocity, int acceleration);
+
+    void step(){}
+
+    bool pre_motion_sanity_checks(float initial_position, float initial_velocity, float
+    goal_position, float max_velocity, float max_acceleration);
+
+    void print_values();
+
+private:
+    int _initial_position;
+    int _initial_velocity;
+    int _current_position;
+    int _current_velocity;
+    int _current_acceleration;
+    int _goal_position;
+    int _max_velocity;
+    int _max_acceleration;
+
+};
 
 };
 
